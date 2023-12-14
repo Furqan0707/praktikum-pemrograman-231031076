e3bf8f913df7b7dde30fbce360e3bf75ac1e1e5e
Furqan0707 <153783484+Furqan0707@users.noreply.github.com>
2023-12-14 08:59:23 +0800

    Upload praktikum 6

---
+++ Praktikum-c6.py
@@ -0,0 +1,13 @@
+import os
+os.system('clear')
+
+a = True
+
+while a:
+    pilih = input('Apakah ingin melanjutkan?[y/n]')
+    if pilih == 'y':
+        print('Terima kasih!')
+    elif pilih == 'n':
+        print('Sampai jumpa lagi!)')
+    else:
+        print('Jangan aneh deh! :(')
